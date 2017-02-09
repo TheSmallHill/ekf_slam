6ec5b2d62be3fa5974551833bcd947351b6b8acb
TheSmallHill <jacob.knoll16@gmail.com>
2017-02-09 15:57:54 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
+#include "motor.h"
 #include "slamLib.h"
 
 using namespace std;
+using namespace BlackLib;
 
 int main(void){
 
 //create gpio pins from blacklib	
-BlackLib::BlackGPIO step = BlackGPIO(BlackLib::GPIO_30, BlackLib::output, BlackLib::FastMode);
-BlackLib::BlackGPIO direc = BlackGPIO(BlackLib::GPIO_60, BlackLib::output, BlackLib::FastMode);
+//BlackLib::BlackGPIO step = BlackGPIO(BlackLib::GPIO_30, BlackLib::output, BlackLib::FastMode);
+//BlackLib::BlackGPIO direc = BlackGPIO(BlackLib::GPIO_60, BlackLib::output, BlackLib::FastMode);
 	
-motor *M1 = new motor(&step, &direc);
+//motor *M1 = new motor(&step, &direc);
 	
 //initializations
 M1->pos=0;
