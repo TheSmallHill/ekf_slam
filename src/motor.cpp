bc408e6c2845e7b4136dfdcf4fe3660b15255525
TheSmallHill <jacob.knoll16@gmail.com>
2017-02-09 12:54:13 -0600

    Update motor.cpp

--- src/motor.cpp
+++ src/motor.cpp
@@ -20,3 +20,35 @@ float motor::getAng()
   return (pose*stepSize)/msDivider;
 
 }
+
+int motor::setDirection(int dir)
+{
+
+  if (dir == 0) { //clockwise
+  
+    directionPin.setValue(low);
+    
+  } else if (dir == 1) {
+  
+    directionPin.setValue(high);
+  
+  } else {
+  
+    cout << "Invalid direction" << endl;
+    
+  }
+  
+  if ((directionPin.isHigh && dir == 1) || (!directionPin.isHigh && dir == 0)) {
+  
+    return(1);
+    
+  } else return(0);
+
+}
+
+int motor::setMicrostep(int* ms[3])
+{
+
+  
+
+}
