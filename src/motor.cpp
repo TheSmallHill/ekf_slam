aec7d8c67e2cc3ceac20a38a464128133ebc4660
TheSmallHill <jacob.knoll16@gmail.com>
2017-02-09 12:06:31 -0600

    Update motor.cpp

--- src/motor.cpp
+++ src/motor.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <stdint.h>
+#include <unistd.h>
+
 #include "motor.h"
+
+using namespace std;
+using namespace BlackLib;
+
+int motor::getPos()
+{
+
+  return pos;
+  
+}
+
+float motor::getAng()
+{
+
+  return (pose*stepSize)/msDivider;
+
+}
