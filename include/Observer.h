b5c99fbe888542201d6bbe8f195795b32f2480ed
TheSmallHill <jacob.knoll16@gmail.com>
2017-02-10 12:26:17 -0600

    Added observation struct
    Added observation struct so all data related to a beacon and angle will be kept together, allowing for easier data processing

--- include/Observer.h
+++ include/Observer.h
@@ -11,24 +11,36 @@
 using namespace std;
 using namespace BlackLib;
 
+// struct to hold a single observation
+typedef struct observation{
+  int ID;
+  float angle;
+  int RSSI;
+  float distance;
+};
+
 class Observer {
 
   public:
     
-    Oberver(); //constructor
+    Oberver(); //constructor, start uart, connect to xbee, create results
     ~Observer(); //destructor
-    doOberservation(); //Start performing an observation, only one
-    newScan(); //prepare for a new set of observations (clear results, reset numObservations)
-    calibrate();
-    getResults();
-    process  
+    void doOberservation(); //Start performing an observation, only one
+    void newScan(); //prepare for a new set of observations (clear results, reset numObservations)
+    void calibrate(int, int);
+    void processData(); // go through results, convert to distance, find range and bearing for each beacon, save to results  
+    observation** getResults(); //returns pointer to the final, processed array
   
   private:
   
-    observationCB(struct xbee*, struct xbee_con*, struct xbee_pkt**, void**);
+    void observationCB(struct xbee*, struct xbee_con*, struct xbee_pkt**, void**);
+    float findBearing(); //find bearing for 1 beacon
+    float toDistance(int); //convert one RSSI to distance
+    
   
   protected:
   
     int numObservations;  
+    observation** observedData; //2D array of observations (array will still be organized by angle and ID but this will make processing easier)
   
 }
