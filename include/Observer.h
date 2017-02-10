2da71f93f9a189083735e75e0b829ef5b1018e8f
TheSmallHill <jacob.knoll16@gmail.com>
2017-02-10 11:49:50 -0600

    Update Observer.h

--- include/Observer.h
+++ include/Observer.h
@@ -1,12 +1,34 @@
+#pragma once
+#include <iostream>
+#include <cstdint>
+#include <stdint.h>
+#include <string.h>
+#include <string>
+
+#include "../lib/BlackLib/v3_0/BlackLib.h"
+#include <xbeep.h>
+
+using namespace std;
+using namespace BlackLib;
+
 class Observer {
 
   public:
     
     Oberver(); //constructor
-    ~Observer();
-    
+    ~Observer(); //destructor
+    doOberservation(); //Start performing an observation, only one
+    newScan(); //prepare for a new set of observations (clear results, reset numObservations)
+    calibrate();
+    getResults();
+    process  
+  
   private:
   
+    observationCB(struct xbee*, struct xbee_con*, struct xbee_pkt**, void**);
+  
   protected:
-
+  
+    int numObservations;  
+  
 }
