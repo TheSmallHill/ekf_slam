90e3b8755bc4b10735ca6a8275342aa9017d058d
TheSmallHill <jacob.knoll16@gmail.com>
2017-02-27 11:43:37 -0600

    Update Slam.h

--- include/Slam.h
+++ include/Slam.h
@@ -1,1 +1,21 @@
 #pragma once
+#include <iostream>
+#include <cstdint>
+#include <stdint.h>
+#include <Eigen/Eigen> /* Eigen library */
+
+class Slam{
+  
+  public:
+    void getObservations();
+    void dataAssociateKnown();
+    void update();
+    void augment();
+    void storeData();
+  
+  protected:  
+    
+  private: 
+    Eigen::Matrix<double, Dynamic, Dynamic, RowMajor> Pcov;  // Fully dynamic, row major (heap allocation) 
+  
+};
