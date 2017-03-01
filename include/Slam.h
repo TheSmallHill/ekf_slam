e8c3da545b71cd4a216bdcf26a4ab46297ad4b01
TheSmallHill <jacob.knoll16@gmail.com>
2017-03-01 17:12:02 -0600

    Update Slam.h
    Updated member declarations, work in progress

--- include/Slam.h
+++ include/Slam.h
@@ -2,20 +2,35 @@
 #include <iostream>
 #include <cstdint>
 #include <stdint.h>
-#include <Eigen/Eigen> /* Eigen library */
+
+#include <eigen3/Eigen/Eigen>
+
+#include "ros/ros.h"
 
 class Slam{
-  
-  public:
-    void getObservations();
-    void dataAssociateKnown();
-    void update();
-    void augment();
-    void storeData();
-  
-  protected:  
-    
-  private: 
-    Eigen::Matrix<double, Dynamic, Dynamic, RowMajor> Pcov;  // Fully dynamic, row major (heap allocation) 
-  
+
+	public:
+		Slam(double*, double, double);
+		~Slam();		
+		void getObservations();
+		void dataAssociateKnown(Eigen::Matrix<double, Eigen::Dynamic, 1, Eigen::RowMajor>, Eigen::Matrix<double, 2, Eigen::Dynamic, Eigen::RowMajor>, Eigen::Matrix<int, Eigen::Dynamic, 1, Eigen::RowMajor>);
+		void predict(double, double);		
+		void update(Eigen::Matrix<double, Eigen::Dynamic, 1, Eigen::RowMajor>, Eigen::Matrix<double, 2, Eigen::Dynamic, Eigen::RowMajor>);
+		void augment();
+		void storeData();
+
+		Eigen::Matrix<int, 1, Eigen::Dynamic, Eigen::RowMajor> da_table;
+
+	protected:
+		
+	private:
+		void choleskyUpdate();
+		Eigen::Matrix<double, 1, Eigen::Dynamic, Eigen::RowMajor> observeModel(Eigen::Matrix<double, Eigen::Dynamic, 1, Eigen::RowMajor>, int, int, double*);		
+		
+		Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> Pcov;
+		double sigmaD, sigmaV, sigmaW, sigmaR, sigmaB, length, timeStep, Vn, Wn;
+		double* pose[3];
+		Eigen::Matrix<double, 2, 2, Eigen::RowMajor> Q, R;
+		Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> zf, idf, zn; 
+
 };
